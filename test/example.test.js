bb57638b64ea7bd72dcbb954b9f7b9d34049fbb1
patrick hrabos <phrabos@gmail.com>
2021-01-19 22:58:47 -0500

    reformat test to take in an actual object from array in data.js

--- test/example.test.js
+++ test/example.test.js
@@ -1,24 +1,17 @@
 // IMPORT MODULES under test here:
 import { renderTea } from '../products/render-tea.js';
+import { oolongTeas } from '../products/data.js';
 
 const test = QUnit.test;
 
 test('should take in a tea and return a li', (expect) => {
-    const aliShan = {
-        id: 2, 
-        name: 'Ali Shan',
-        image: 'alishan.jpg',
-        description: 'floral lightly oxidized from Ali Mountain in Taiwan',
-        category: 'Taiwanese Oolong',
-        price: 35,
-    };
     //Arrange
     // Set up your arguments and expectations
-    const expected = `<li class="tea-item"><h3 class="tea-name">Ali Shan</h3><img src="../assets/alishan.jpg" class="tea-image"><p class="tea-category">Taiwanese Oolong</p><p class="tea-description">floral lightly oxidized from Ali Mountain in Taiwan</p><p class="tea-price">$35.00</p><button value="2">Add to Cart</button></li>`;
+    const expected = `<li class="tea-item"><h3 class="tea-name">Ali Shan</h3><img src="../assets/alishan.jpg" class="tea-image"><p class="tea-category">Taiwanese Oolong</p><p class="tea-description">floral lightly oxidized from Ali Mountain</p><p class="tea-price">$35.00</p><button value="2">Add to Cart</button></li>`;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = renderTea(aliShan);
+    const actual = renderTea(oolongTeas[1]);
 
     //Expect
     // Make assertions about what is expected versus the actual result
